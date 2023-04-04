8051fa8cfa122528c7d6d280d800e96efc07af98
Saif Ansari <sansari@awesomemotive.com>
2023-04-04 10:47:58 +0530

    eslint, editor config setup

--- database/sql_connection.js
+++ database/sql_connection.js
@@ -1,12 +1,12 @@
-const mysql = require('mysql')
+const mysql = require('mysql');
 
-const    connection_sql = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'root',
-    database:'event'
-})
+const connectionSql = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'event',
+});
 
-module.exports={
-    connection_sql
-}
+module.exports = {
+  connectionSql,
+};
